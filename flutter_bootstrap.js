4f0e7d4c382d2800cfa276d0ef26ff910a118d8b
18-anth <anthonycordova330@gmail.com>
2026-08-03 22:50:03 -0500

    config web stock.json

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2607683638"
+    serviceWorkerVersion: "2341314433"
   }
 });
