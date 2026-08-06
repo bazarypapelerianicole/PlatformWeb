070fad63e251750dae8b951a4e3b2850994ae657
18-anth <anthonycordova330@gmail.com>
2026-08-05 19:04:23 -0500

    config index

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "658977364"
+    serviceWorkerVersion: "3373967067"
   }
 });
